d2c2e15d17b0fdfa4d68496786bc5e5d1a9972fe
Marco Hilpert <marcohilpert@Marcos-Air.home>
2020-03-24 21:51:40 +0100

    The increment and Decrement Operator

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -33,3 +33,9 @@ console.log('The value of levelUp:', levelUp);
 console.log('The value of powerLevel:', powerLevel); 
 console.log('The value of multiplyMe:', multiplyMe); 
 console.log('The value of quarterMe:', quarterMe);
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+lostDollar--;
